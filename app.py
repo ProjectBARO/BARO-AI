18fc064abffa9bf494efbe229133e3ad92362ade
fbckdgns3@naver.com <fbckdgns3@naver.com>
2024-01-09 15:48:29 +0900

    [#11] fix: api 출력결과 JSON 형태로 변경

--- app.py
+++ app.py
@@ -2,27 +2,29 @@ import os
 import requests
 import tempfile
 import tensorflow as tf
-from flask import Flask, render_template, request
 import cv2
 import numpy as np
+from flask import Flask, render_template, request, jsonify
 
 
-
+# TensorFlow Eager Execution 활성화
 tf.config.run_functions_eagerly(True)
 
 app = Flask(__name__)
 
 
+# 모델 불러오기 및 컴파일
 model = tf.keras.models.load_model('CNN_model.h5')
 model.compile(run_eagerly=True)
 
+
 def extract_frames(video_file, interval=5):
     cap = cv2.VideoCapture(video_file)
-    frameRate = cap.get(5) # 프레임 5초 단위
+    frameRate = cap.get(5)  # 프레임 5초 단위
     images = []
 
     while cap.isOpened():
-        frameId = cap.get(1) # 현재 프레임 번호
+        frameId = cap.get(1)  # 현재 프레임 번호
         ret, frame = cap.read()
         if not ret:
             break
@@ -42,20 +44,22 @@ def download_video(url):
             return tmp_file.name
     return None
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     try:
-        video_url = request.args.get('video_url')
+        video_url = request.args.get('video_url') if request.method == 'GET' else request.form.get('video_url')
         if not video_url:
-            raise Exception("No video URL provided")
+            raise ValueError("No video URL provided")
 
         video_file = download_video(video_url)
         if video_file is None:
-            raise Exception("Video download failed")
+            raise ValueError("Video download failed")
 
         images = extract_frames(video_file)
         predictions = model.predict(images)
@@ -63,10 +67,12 @@ def predict():
 
         os.remove(video_file)
 
-        return render_template('index.html', result=result.tolist())
+        return jsonify({'result': result.tolist()})
 
     except Exception as e:
-        return render_template('index.html', result=str(e))
+        return jsonify({'error': str(e)}), 400  
+    
 
+    
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
